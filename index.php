ed4d3bacd4fb03b81bed85150a09d271e61e4c3a
Carlos <carlos_garnacho@hotmail.com>
2015-08-27 11:14:19 +0200

    uso de includes y paso a php pages

--- index.php
+++ index.php
@@ -13,38 +13,10 @@
         <!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
     </head>
     <body>
-        <!-- Navbar -->
-        <nav role="navigation" class="navbar navbar-fixed-top">
-            <div class="navbar-header">
-                <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle glyphicon glyphicon-menu-hamburger">
-                </button>
-                <a href="#" class="navbar-brand">CGB</a>
-            </div>
-
-            <div id="navbarCollapse" class="collapse navbar-collapse">
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="#header" class="scrolly" data-target="#navbarCollapse" data-toggle="collapse">Inicio</a></li>
-                    <li><a href="#sobremi" class="scrolly" data-target="#navbarCollapse" data-toggle="collapse">Sobre mí</a></li>
-                    <li><a href="#formacionacademica" class="scrolly" data-target="#navbarCollapse" data-toggle="collapse">Formación académica</a></li>
-                    <li class="dropdown">
-                        <a data-toggle="dropdown" class="dropdown-toggle" href="#">Mis proyectos <b class="caret"></b></a>
-                        <ul role="menu" class="dropdown-menu">
-                            <li><a class="elemento" href="#">Mi web personal</a></li>
-                            <li><a class="elemento" href="findyourmall.html">Find Your Mall</a></li>
-                            <li><a class="elemento" href="#">Mas proyectos...</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="#footer" class="scrolly" data-target="#navbarCollapse" data-toggle="collapse">Footer</a></li>
-                </ul>
-                <!--<form role="search" class="navbar-form navbar-right">
-                   <div class="form-group">
-                        <input type="text" placeholder="Search" class="form-control">
-                   </div>
-               </form>-->
-            </div>
-        </nav>
-
-
+        <?php
+            include "footer.php";
+        ?>
+        
         <!-- Header -->
         <section id="header">
             <div class="inner">
---
+++ navbar.php
@@ -0,0 +1,31 @@
+        <!DOCTYPE HTML>
+        <!-- Navbar -->
+        <nav role="navigation" class="navbar navbar-fixed-top">
+            <div class="navbar-header">
+                <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle glyphicon glyphicon-menu-hamburger">
+                </button>
+                <a href="#" class="navbar-brand">CGB</a>
+            </div>
+
+            <div id="navbarCollapse" class="collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#header" class="scrolly" data-target="#navbarCollapse" data-toggle="collapse">Inicio</a></li>
+                    <li><a href="#sobremi" class="scrolly" data-target="#navbarCollapse" data-toggle="collapse">Sobre mí</a></li>
+                    <li><a href="#formacionacademica" class="scrolly" data-target="#navbarCollapse" data-toggle="collapse">Formación académica</a></li>
+                    <li class="dropdown">
+                        <a data-toggle="dropdown" class="dropdown-toggle" href="#">Mis proyectos <b class="caret"></b></a>
+                        <ul role="menu" class="dropdown-menu">
+                            <li><a class="elemento" href="#">Mi web personal</a></li>
+                            <li><a class="elemento" href="findyourmall.html">Find Your Mall</a></li>
+                            <li><a class="elemento" href="#">Mas proyectos...</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#footer" class="scrolly" data-target="#navbarCollapse" data-toggle="collapse">Footer</a></li>
+                </ul>
+                <!--<form role="search" class="navbar-form navbar-right">
+                   <div class="form-group">
+                        <input type="text" placeholder="Search" class="form-control">
+                   </div>
+               </form>-->
+            </div>
+        </nav>
